e132693feccbea61293456f936a2b383b8c34a63
Samuel Candido <samuca235.pires@gmail.com>
2020-01-14 17:09:32 -0300

    Nova modifição

--- Estudos/Program.cs
+++ Estudos/Program.cs
@@ -12,14 +12,7 @@ namespace Estudos
     {
         static void Main(string[] args)
         {
-            DateTime d1 = new DateTime(2000, 8, 15, 13, 5, 58, DateTimeKind.Local);
-            DateTime d2 = new DateTime(2000, 8, 15, 13, 5, 58, DateTimeKind.Utc);
-            DateTime d3 = new DateTime(2000, 8, 15, 13, 5, 58);
-
-            Console.WriteLine("d1:         " + d1);
-            Console.WriteLine("d1 kind     " + d1.Kind);
-            Console.WriteLine("d1 to Local:" + d1.ToLocalTime());
-            Console.WriteLine("d1 to Utc   " + d1.ToUniversalTime());
+            Console.WriteLine("Dia 14-01-2020 17:06");
 
         }
     }
